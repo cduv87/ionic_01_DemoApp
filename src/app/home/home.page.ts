2e6b879f7484636268dfce3b828056e74196013b
Clément Duval <103181914+cduv87@users.noreply.github.com>
2022-09-12 13:53:26 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  hello: string = "Salut Monde !";
+  nb: number = 0;
+  isHidden: boolean = true;
+  name: string = "gros-minet";
+  fruits: string[] = ["Banane", "Mûre", "Prunelle", "Pomme", "Amande"]
+  colorText: string = "red"
   constructor() {}
+
+  clickMe() {
+    this.hello = "Ce tombeau sera votre tombeau";
+    this.nb++;
+    this.isHidden = !this.isHidden;
+  }
 
 }
